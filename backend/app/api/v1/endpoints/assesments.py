c7e5fb0f6006bd25f70289327ceeefac8c296c6c
3nd3r1 <ender.spam.guru@gmail.com>
2025-11-15 11:09:33 +0200

    feat(assesments): create template endpoints

---
+++ backend/app/api/v1/endpoints/assesments.py
@@ -0,0 +1,39 @@
+from fastapi import APIRouter
+
+
+router = APIRouter()
+
+
+@router.post("")
+def create_assesment():
+    return "Assesment created"
+
+
+@router.get("")
+def get_assesments():
+    return "List of assesments"
+
+
+@router.get("/{assesment_id}")
+def get_assesment(assesment_id: int):
+    return f"Details of assesment {assesment_id}"
+
+
+@router.get("/{assesment_id}/status")
+def get_assesment_status(assesment_id: int):
+    return f"Details of assesment {assesment_id}"
+
+
+@router.delete("/{assesment_id}")
+def delete_assesment(assesment_id: int):
+    return f"Assesment {assesment_id} deleted"
+
+
+@router.put("/{assesment_id}/report")
+def generate_assesment_report(assesment_id: int):
+    return f"Report for assesment {assesment_id} generated"
+
+
+@router.put("/{assesment_id}/report/export?format={export_format}")
+def export_assesment_report(assesment_id: int, export_format: str):
+    return f"Report for assesment {assesment_id} exported in {export_format} format"
--- backend/app/api/v1/routes.py
+++ backend/app/api/v1/routes.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from app.api.v1.endpoints import health
+from app.api.v1.endpoints import assesments, health
 
 
 router = APIRouter()
 
 router.include_router(health.router, tags=["health"])
+router.include_router(assesments.router, prefix="/assesments", tags=["assesments"])
